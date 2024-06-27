8767a200cc5165d68cc701d6b582283add5f7d53
acellle <acellekrislette@gmail.com>
2024-06-27 20:18:45 +0800

    Added template for dashboard and home

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import SignUp from "./components/signup";
+import LogIn from "./components/login";
+
+import Dashboard from "./components/dashboard";
+import Home from "./components/home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="login" element={<LogIn/>}/>
           <Route path="/signup" element={<SignUp/>}/>
+
+          <Route path='/' element={<Dashboard/>}>
+            <Route path='' element={<Home/>}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
